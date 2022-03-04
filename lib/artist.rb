8ad5d50f172c4bb3ba75ecd8400f582bc6dd21a7
Catherine Loesch <catherine.loesch@protonmail.com>
2022-03-04 05:10:34 +0000

    lab completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,11 @@
-require 'pry'
-require_relative './concerns/memorable.rb' 
-require_relative './concerns/findable.rb' 
-require_relative './concerns/paramable.rb' 
-
-
 class Artist
   attr_accessor :name
   attr_reader :songs
 
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-
-  include Paramable::InstanceMethods
   include Memorable::InstanceMethods
-
+  include Paramable::InstanceMethods
 
   @@artists = []
 
@@ -34,5 +26,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,11 @@
-require 'pry'
-require_relative './concerns/memorable.rb' 
-require_relative './concerns/findable.rb'
-require_relative './concerns/paramable.rb' 
-
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-
-  include Paramable::InstanceMethods
   include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
@@ -22,5 +16,4 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
 end
